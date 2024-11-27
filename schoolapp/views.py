70f66b38a9b9fbfe630f1893982fbf2b69da43fd
salva66 <henrymkama@gmail.com>
2024-11-27 19:36:32 +0300

    commit_login

--- schoolapp/views.py
+++ schoolapp/views.py
@@ -14,17 +14,23 @@ def doLogin(request):
     if request.method!="POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        user=EmailBackEnd.authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
         if user!=None:
-            login(request, user)
-            return HttpResponse("/admin_home")
+            login(request,user)
+            if user.user_type=="1":
+                return HttpResponseRedirect('admin_home')
+            elif user.user_type=="2":
+                return HttpResponseRedirect(reverse("staff_home"))
+            else:
+                return HttpResponseRedirect(reverse("student_home"))
         else:
-            messages.error(request, "Invalid Login Details")
+            messages.error(request,"Invalid Login Details")
             return HttpResponseRedirect("/")
+
 
 def GetUserDetails(request):
     if request.user!=None:
-        return HttpResponse("User : "+request.user.email+" usertype : "+request.user.user_type)
+        return HttpResponse("User : "+request.user.email+" usertype : "+str(request.user.user_type))
     else:
         return HttpResponse("Please Login First")
 
