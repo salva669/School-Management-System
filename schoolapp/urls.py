a1ef3e090458bac88b436e26754c91b4591a38e0
salva66 <henrymkama@gmail.com>
2024-11-27 09:45:43 +0300

    commit admin_page

--- schoolapp/urls.py
+++ schoolapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from . import views
+from schoolapp import views, HodViews
+from school import settings
 
 urlpatterns = [
-    path('', views.LoginPageView, name='login'),
+    path('', views.ShowLoginPage, name='login_page'),
+    path('get_user_details', views.GetUserDetails),
+    path('logout_user', views.logout_user,name="logout"),
+    path('doLogin',views.doLogin,name="do_login"),
+    path('admin_home',HodViews.admin_home,name="admin_home"),
+    path('add_staff',HodViews.add_staff,name="add_staff"),
+    path('add_staff_save',HodViews.add_staff_save,name="add_staff_save"),
+    path('add_course',HodViews.add_course,name="add_course"),
+    path('add_course_save',HodViews.add_course_save,name="add_course_save"),
 ]
--- schoolapp/views.py
+++ schoolapp/views.py
@@ -1,5 +1,33 @@
+import datetime
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from schoolapp.EmailBackEnd import EmailBackEnd
 
 # Create your views here.
-def LoginPageView(request):
-    return render(request, 'login.html')
+def ShowLoginPage(request):
+    return render(request, 'login_page.html')
+
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user=EmailBackEnd.authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))
+        if user!=None:
+            login(request, user)
+            return HttpResponse("/admin_home")
+        else:
+            messages.error(request, "Invalid Login Details")
+            return HttpResponseRedirect("/")
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User : "+request.user.email+" usertype : "+request.user.user_type)
+    else:
+        return HttpResponse("Please Login First")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
