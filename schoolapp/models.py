536f6fbb29818e6d3746154ef5a149281c8ac8e0
salva66 <henrymkama@gmail.com>
2024-12-01 08:00:22 +0300

    commit_add_student

--- schoolapp/models.py
+++ schoolapp/models.py
@@ -123,22 +123,21 @@ class NotificationStaffs(models.Model):
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
 
-@receiver(post_save, sender=CustomUser)
-def create_user_profile(sender, instance, created, **kwargs):
+@receiver(post_save,sender=CustomUser)
+def create_user_profile(sender,instance,created,**kwargs):
     if created:
         if instance.user_type==1:
             AdminHOD.objects.create(admin=instance)
         if instance.user_type==2:
-            Staffs.objects.create(admin=instance)
+            Staffs.objects.create(admin=instance,address="")
         if instance.user_type==3:
-            Students.objects.create(admin=instance)
+            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_start_year="2020-01-01",session_end_year="2021-01-01",address="",profile_pic="",gender="")
 
-@receiver(post_save, sender=CustomUser)
-def save_user_profile(sender, instance, **kwargs):
+@receiver(post_save,sender=CustomUser)
+def save_user_profile(sender,instance,**kwargs):
     if instance.user_type==1:
         instance.adminhod.save()
     if instance.user_type==2:
         instance.staffs.save()
     if instance.user_type==3:
         instance.students.save()
-    
